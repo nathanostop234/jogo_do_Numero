265701f649b338576449bedec4d7fc0c617d953a
nathanostop234 <163007786+nathanostop234@users.noreply.github.com>
2024-10-21 11:22:01 -0300

    .

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite');
 let contagemPalpites = 1;
 let botaoReiniciar;
+
+function verificarPalpite() {
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagemPalpites === 1) {
+        palpites.textContent = "Palpites anteriores: ";
+    }
+
+palpites.textContent += palpiteUsuario + " ";
+
+    if (palpiteUsuario === numeroAleatorio) {
+        ultimoResultado.textContent = "Parebéns! Você acertou!";
+        ultimoResultado.style.backgroundColor = "green";
+        baixoOuAlto.textContent = "";
+        finalizarJogo();
+    } else if (contagemPalpites === 10) {
+}
